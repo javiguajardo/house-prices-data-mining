da398533b92dc1af52556d5677b0c513bce30add
guajardo <a282057@uach.mx>
2017-04-09 15:40:47 -0600

    Replace missing values with mean

--- data_cleaning.py
+++ data_cleaning.py
@@ -6,14 +6,22 @@ def open_file(file_name):
     return data
 
 def replace_missing_values_with_mode(data):
-    features = data[['Alley', 'BsmtFinType2', 'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath', 'BedroomAbvGr', 'Fireplaces', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageCars', 'GarageQual', 'GarageCond', 'Fence', 'MiscFeature']]
+    features = data[['Alley', 'BsmtFinType2', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 'Fence', 'MiscFeature']]
     columns = features.columns
     mode = data[columns].mode()
     data[columns] = data[columns].fillna(mode.iloc[0])
 
     return data
 
+def replace_missing_values_with_mean(data):
+    features = data['LotFrontage']
+    mean = features.mean()
+    data['LotFrontage'] = data['LotFrontage'].fillna(mean)
+
+    return data
+
 if __name__ == '__main__':
     data = open_file('../resources/train.csv')
     replace_missing_values_with_mode(data)
-    print(data)
+    replace_missing_values_with_mean(data)
+    print(data[:10])
